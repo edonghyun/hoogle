de001bf723ccf18b79900a824804eb190698486b
donghyun <edonghyun@naver.com>
2021-10-11 12:22:04 +0900

    add webpack & babel configurations

---
+++ frontend/react/webpack.config.js
@@ -0,0 +1,41 @@
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+    mode: 'development',
+    entry: './src/index.js',
+    output: {
+        filename: 'bundle.[chunkhash].js',
+    },
+    devServer: {
+        host: process.env.HOST || 'localhost',
+        port: process.env.PORT || 3332,
+        open: true,
+    },
+    plugins: [new MiniCssExtractPlugin({ filename: 'css/style.css' })],
+    module: {
+        rules: [
+            {
+                test: /\.scss$/,
+                use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+            },
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                },
+            },
+            {
+                test: /\.html$/,
+                use: [
+                    {
+                        loader: 'html-loader',
+                        options: {
+                            minimize: true,
+                        },
+                    },
+                ],
+            },
+        ],
+    },
+};
